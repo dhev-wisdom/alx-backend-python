06f1f8c3bf73f6c39e2d72dc057b219ca24fd467
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-03 19:51:36 +0100

    to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module demostrates basic Python variable annotation
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    typed python function
+    """
+    return str(n)
