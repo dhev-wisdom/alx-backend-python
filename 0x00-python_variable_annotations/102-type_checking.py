f7a2519d542b783b9e0ace9fee9b2999ed5e4379
dhev-wisdom <nonsowisdom@gmail.com>
2023-08-03 18:37:38 -0700

    Check

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,6 +6,7 @@ Module demotrates typing in Python
 
 from typing import Tuple, List
 
+
 def zoom_array(lst: Tuple[int], factor: int = 2) -> List[int]:
     zoomed_in: List[int] = [
         item for item in lst
