54343942e65c1f2d991949a3041a3340cf865e6e
dhev-wisdom <nonsowisdom@gmail.com>
2023-08-03 18:34:13 -0700

    Check

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,7 +5,7 @@ Module demotrates typing in Python
 from typing import Iterable, Sequence, Any, Union
 
 
-def safe_first_element(lst: Iterable[Sequence]) -> Union[Any,  None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any,  None]:
     if lst:
         return lst[0]
     else:
---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module demotrates typing in Python
+"""
+from typing import Any, Union, Optional, TypeVar, Mapping
+
+
+
+T = TypeVar('T')
+
+def safely_get_value(dct: Mapping, key: Any, default: Optional[T] = None) -> Union[Any, T]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
