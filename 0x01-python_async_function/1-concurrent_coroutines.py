763cc9db4fa599cae6a84a408bcb550fde5f0082
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-07 19:31:05 +0100

    check checker

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,10 +7,14 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> int:
     """
     async function
     """
     i = random.uniform(0, max_delay)
     await asyncio.sleep(i)
     return (i)
+
+
+if __name__ == '__main__':
+    wait_random()
