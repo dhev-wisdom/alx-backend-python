73c6dbcf323a36e7537167d831c4842596bcdda6
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-07 23:11:51 +0100

    asyncio.gather

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,16 +5,13 @@ Module demostrates Asynchronous programming in Python
 
 import asyncio
 import random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_random(max_delay: int = 10) -> int:
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """
     async function
     """
-    i = random.uniform(0, max_delay)
-    await asyncio.sleep(i)
-    return (i)
-
-
-if __name__ == '__main__':
-    wait_random()
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
