ccac70f9248f1271746460c2951d13ec58ae8681
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-07 23:34:21 +0100

    Check 2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,16 +2,18 @@
 """
 Module demostrates Asynchronous programming in Python
 """
-
+import asyncio
 import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> float:
+async def measure_time(n: int, max_delay: int) -> float:
     """
     async function
     """
-    start_time = time.perf_counter()
-    wait_n(n, max_delay)
-    total_time = time.perf_counter() - start_time
-    return total_time / n
+    start = time.perf_counter()
+    await wait_n(n, max_delay)
+    end = time.perf_counter()
+    total = end - start
+    average = total / n
+    return average
