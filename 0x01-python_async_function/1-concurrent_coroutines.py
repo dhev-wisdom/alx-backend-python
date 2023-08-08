4ffd2a0d768c68fb0b4c626bff9e1d6b63cfbcf8
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-08 05:21:08 +0100

    check 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,10 +5,11 @@ Module demostrates Asynchronous programming in Python
 
 import asyncio
 import random
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     async function
     """
