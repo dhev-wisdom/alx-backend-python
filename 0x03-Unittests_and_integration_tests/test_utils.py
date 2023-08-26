7abadc4b9f40cc338b764faf87dd8ca2bc3f8548
dhev-wisdom <nonsowisdom62@gmail.com>
2023-08-26 18:28:17 +0100

    asertRaises

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -25,6 +25,19 @@ class TestAccessNestedMap(unittest.TestCase):
         result = access_nested_map(param_1, param_2)
         self.assertEqual(result, expected)
 
+    @parameterized.expand([
+        ({}, ("a",), KeyError("a")),
+        ({"a": 1}, ("a", "b"), KeyError("b")),
+        ])
+    def test_access_nested_map_exception(self, param_1, param_2, expected):
+        """
+        Use the assertRaises context manager to test that
+        a KeyError is raised for the following inputs
+        """
+        with self.assertRaises(KeyError):
+            result = access_nested_map(param_1, param_2)
+            self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unitest.main()
