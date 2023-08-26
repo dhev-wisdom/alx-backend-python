5d9f6b0f9bec0f51e1e4160120fc3b73b4f13f5a
dhev-wisdom <nonsowisdom62@gmail.com>
2023-08-26 18:12:33 +0100

    unittest with parameterized decorator

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,11 +14,11 @@ class TestAccessNestedMap(unittest.TestCase):
     and performs unittest with @parameterized.expand decorator
     """
     @parameterized.expand([
-            ("first", {"a": 1}, ("a",), 1),
-            ("second", {"a": {"b": 2}}, ("a",), {"b": 2}),
-            ("third", {"a": {"b": 2}}, ("a", "b"), 2),
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
             ])
-    def test_access_nested_map(self, name, param_1, param_2, expected):
+    def test_access_nested_map(self, param_1, param_2, expected):
         """
         method to test that the method returns what it is supposed to.
         """
