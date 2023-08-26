bb94174d39ca9feb21d13e465539faa827795819
dhev-wisdom <nonsowisdom62@gmail.com>
2023-08-26 18:05:59 +0100

    unittest with parameterized decorator

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,7 @@
 Testing a function from a python script
 """
 
+from parameterized import parameterized
 import unittest
 access_nested_map = __import__("utils").access_nested_map
 
@@ -12,14 +13,17 @@ class TestAccessNestedMap(unittest.TestCase):
     class that inherits from unittest.TestCase
     and performs unittest with @parameterized.expand decorator
     """
-    # @parameterized.expand
-    def test_access_nested_map(self):
+    @parameterized.expand([
+            ("first", {"a": 1}, ["a"], 1),
+            ("second", {"a": {"b": 2}}, ["a"], {"b": 2}),
+            ("third", {"a": {"b": 2}}, ["a", "b"], 2),
+            ])
+    def test_access_nested_map(self, name, param_1, param_2, expected):
         """
         method to test that the method returns what it is supposed to.
         """
-        self.assertEqual(access_nested_map({"a": 1}, ["a"]), 1)
-        self.assertEqual(access_nested_map({"a": {"b": 2}}, ["a"]), {"b": 2})
-        self.assertEqual(access_nested_map({"a": {"b": 2}}, ["a", "b"]), 2)
+        result = access_nested_map(param_1, param_2)
+        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
