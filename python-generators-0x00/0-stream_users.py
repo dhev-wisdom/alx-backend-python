2511eaac7ac8ae1799be5a8e96feef1e2f16f3f3
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-07-06 21:46:53 +0100

    check

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -3,13 +3,12 @@
 seed = __import__('seed')
 
 def stream_users():
-    connection = seed.connect_db()
+    connection = seed.connect_to_prodev()
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM user_data")
     for row in cursor:
         yield row
     cursor.close()
-    connection.close()
+    connection.close
 
-if __name__ == "__main__":
-    users = stream_users()
+stream_users = stream_users
---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+seed = __import__("seed")
+
+def stream_users_in_batches(batch_size):
+    """function fetches rows in batches of `batch_size`"""
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor()
+    total_rows = 0
+    offset = 0
+    batch_num = 1
+    
+    while True:
+        cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s", (batch_size, offset))
+        batch = cursor.fetchall()
+        if not batch:
+            break
+        yield batch
+        offset += batch_size
+        total_rows += 1
+        batch_num += 1
+
+    cursor.close()
+    connection.close()
+
+def batch_processing(batch_size):
+    """function processes each batch to filter users over the age of25"""
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            user_id, name, email, age = user
+            if age > 25:
+                print(user)
+
+if __name__ == "__main__":
+    batch_processing_ = batch_processing(50)
