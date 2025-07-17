b4f83fa53f4c3a8bfe5849f4688db061dadd182f
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-07-17 12:43:33 +0100

    modify models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
+import uuid
 
 class User(AbstractUser):
     """custom `User` class defined off of django default user `AbstractUser`"""
+    user_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    password = models.CharField(max_length=128, verbose_name="Password (Hashed)")
     email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         """define the string representation of the custom `User` model"""
@@ -18,6 +27,7 @@ class Conversation(models.Model):
     Class serves as a room where conversations hold.
     Many User(s) (at least 2) can send messages here.
     """
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     participants = models.ManyToManyField(User, related_name="conversations")
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -32,14 +42,15 @@ class Message(models.Model):
     Message model that defines the unit of every Conversation
     Each Message belong to only one Conversation
     """
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
-    body = models.TextField()
+    message_body = models.TextField()
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="messages")
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
+    sent_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
     is_modified = models.BooleanField(default=False)
 
     def __str__(self):
         """define the string representation of the `Message` model"""
-        return f"{self.sender.username} - {self.body[:15]}"
+        return f"{self.sender.username} - {self.message_body[:15]}"
     
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -58,9 +58,7 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.isAuthenticated',
-    ],
+    'DEFAULT_PERMISSION_CLASSES': ["rest_framework.permissions.IsAuthenticated"],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
