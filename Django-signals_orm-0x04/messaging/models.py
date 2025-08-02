c389393888868008046283116d1442509e5e4fb0
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-08-02 22:23:36 +0100

    custom model manager- code organization

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from .managers import UnreadMessagesManager
 
 User = get_user_model()
 
-
-class UnreadMessagesManager(models.Manager):
-    """Custom manager to filter unread messages for a user"""
-    def for_user(self, user):
-        return self.get_queryset().filter(receiver=user, edited=False).only(
-            "id", "sender", "receiver", "content", "timestamp"
-        )
 
 class Message(models.Model):
     """Message Model"""
